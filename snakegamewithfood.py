5ec89414b872f89f165f5733077180b06904e7bb
Anukul Banjade <91711407+Anukulbanjade@users.noreply.github.com>
2022-08-20 15:38:50 +0545

    upload
    new file with major update

--- snakegamewithfood.py
+++ snakegamewithfood.py
@@ -1,5 +1,7 @@
-import random
 import pygame
+import random
+import os
+from pygame.locals import *
 
 pygame.init()
 
@@ -12,63 +14,147 @@ black = (0, 0, 0)
 screen_width = 900
 screen_height = 600
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
+bgimg=pygame.image.load('back.png')
+bgimg=pygame.transform.scale(bgimg,(screen_width,screen_height)).convert_alpha()
 
-pygame.display.set_caption("----Snakes----")
+
+# Game Title
+pygame.display.set_caption("**SNAKES**")
 pygame.display.update()
-
-# Game specific variables
-exit_game = False
-game_over = False
-snake_x = 45
-snake_y = 55
-velocity_x = 0
-velocity_y = 0
-score = 0
-snake_size = 30
-fps = 30
-food_x=0
-food_y=0
-
-
 clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 55)
+
+def text_screen(text, color, x, y):
+    screen_text = font.render(text, True, color)
+    gameWindow.blit(screen_text, [x,y])
+
+
+def plot_snake(gameWindow, color, snk_list, snake_size):
+    for x,y in snk_list:
+        pygame.draw.rect(gameWindow, color, [x, y, snake_size, snake_size])
+
+def welcome():
+    exit_game=False
+    while not exit_game:
+        gameWindow.fill(black)
+        text_screen("Welcome To Snakes",white,260,100)
+        text_screen("Press Space To Play",white,260,250)
+        text_screen("Highscores",white,320,300)
+
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                exit_game=True
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_SPACE:
+                    gameloop()
+
+
+        pygame.display.update()
+        clock.tick(60)
+
+
 
 # Game Loop
-while not exit_game:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit_game = True
+def gameloop():
+    # Game specific variables
+    exit_game = False
+    game_over = False
+    snake_x = 45
+    snake_y = 55
+    velocity_x = 0
+    velocity_y = 0
+    snk_list = []
+    snk_length = 1
+    if (not os.path.exists("highscore.txt")):
+        with open("highscore.txt","w") as f:
+            f.write("0")
+    with open("highscore.txt", "r") as f:
+        highscore = f.read()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                velocity_x = 5
-                velocity_y = 0
+    food_x = random.randint(20, screen_width / 2)
+    food_y = random.randint(20, screen_height / 2)
+    score = 0
+    init_velocity = 5
+    snake_size = 30
+    fps = 60
 
-            if event.key == pygame.K_LEFT:
-                velocity_x = - 5
-                velocity_y = 0
+    while not exit_game:
+        if game_over:
+            with open("highscore.txt", "w") as f:
+                f.write(str(highscore))
+            gameWindow.fill(white)
+            text_screen("Game Over! Press Enter To Continue", red, 100, 250)
 
-            if event.key == pygame.K_UP:
-                velocity_y = - 5
-                velocity_x = 0
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit_game = True
 
-            if event.key == pygame.K_DOWN:
-                velocity_y = 5
-                velocity_x = 0
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        welcome()
 
-    snake_x = snake_x + velocity_x
-    snake_y = snake_y + velocity_y
 
-    if abs(snake_x - food_x) < 6 and abs(snake_y - food_y < 6):
-        score += 1
-        print("Score= ", score)
-        food_x = random.randint(20, screen_width / 2)
-        food_y = random.randint(20, screen_height / 2)
+        else:
 
-    gameWindow.fill(black)
-    pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
-    pygame.draw.rect(gameWindow,white, [snake_x, snake_y, snake_size, snake_size])
-    pygame.display.update()
-    clock.tick(fps)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    exit_game = True
 
-pygame.quit()
-quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        velocity_x = init_velocity
+                        velocity_y = 0
+
+                    if event.key == pygame.K_LEFT:
+                        velocity_x = - init_velocity
+                        velocity_y = 0
+
+                    if event.key == pygame.K_UP:
+                        velocity_y = - init_velocity
+                        velocity_x = 0
+
+                    if event.key == pygame.K_DOWN:
+                        velocity_y = init_velocity
+                        velocity_x = 0
+
+                    if event.key==pygame.K_q:
+                       score+=5
+
+            snake_x = snake_x + velocity_x
+            snake_y = snake_y + velocity_y
+
+            if abs(snake_x - food_x)<6 and abs(snake_y - food_y)<6:
+                score +=10
+                food_x = random.randint(20, screen_width / 2)
+                food_y = random.randint(20, screen_height / 2)
+                snk_length +=5
+                if score>int (highscore):
+                    highscore=score
+
+            gameWindow.fill(white)
+            gameWindow.blit(bgimg,(0,0))
+            text_screen("Score: " + str(score) +"            highscore:"+str(highscore), red, 5, 5)
+            pygame.draw.rect(gameWindow, red, [food_x, food_y, snake_size, snake_size])
+
+
+            head = []
+            head.append(snake_x)
+            head.append(snake_y)
+            snk_list.append(head)
+
+            if len(snk_list)>snk_length:
+                del snk_list[0]
+
+            if head in snk_list[:-1]:
+                game_over = True
+
+            if snake_x<0 or snake_x>screen_width or snake_y<0 or snake_y>screen_height:
+                game_over = True
+            plot_snake(gameWindow, black, snk_list, snake_size)
+        pygame.display.update()
+        clock.tick(fps)
+
+    pygame.quit()
+    quit()
+welcome()
+
